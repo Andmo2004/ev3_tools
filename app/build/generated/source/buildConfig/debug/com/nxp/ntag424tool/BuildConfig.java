266b6f23b4c83a550ca695a1eff729b2ecb81f07
Andmo2004 <115933077+Andmo2004@users.noreply.github.com>
2026-03-10 09:52:31 +0000

    v1.12

--- app/build/generated/source/buildConfig/debug/com/nxp/ntag424tool/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/nxp/ntag424tool/BuildConfig.java
@@ -10,5 +10,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Field from default config.
-  public static final String TAPLINX_KEY = "db3feae1d9d97444abfaa38aa57c2196";
+  public static final String TAPLINX_KEY = "5959b36bc5793421f46792773f179fa1";
 }
