fe46d102ee0282c417229ca654e3deb543591a6d
Andmo2004 <115933077+Andmo2004@users.noreply.github.com>
2026-03-10 10:01:53 +0000

    v1.13

--- app/build/generated/source/buildConfig/debug/com/nxp/ntag424tool/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/nxp/ntag424tool/BuildConfig.java
@@ -10,5 +10,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
   // Field from default config.
-  public static final String TAPLINX_KEY = "5959b36bc5793421f46792773f179fa1";
+  public static final String TAPLINX_KEY = "pmHc+9ACbu+JGvPgXl3kCq5pL/dV8Bi6iPQPCMl2YLcW7Rp2+Tku5j63LWzlp0wx0Kc2YBjWNG1zSmEmk+EmLnZheF610JYNEejpLmmIZzZSuWKeMJeoADt9gBFTOgzAOt4XCYUTodRVJze6Kc2fimCwoBXJsBTICdf2F5yvhDhKhklvZF61Mf9s+TLDWRN0Uqf1wE6K0CyRD0rBlmSzxsgAfRA1nqwmKmUIH1GeKPX360v8z6QkXpDm/ajDwdXQJK2CZQT4uNDzX9GVAAInCQiAFLIjaSds4SEQoJBdx/0vRzGssHmVa+jGCVB6bOT8Td7Qs68v5cUq6WDCNSAuEQ==";
 }
